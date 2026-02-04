03e6016f067e4059b0f24811b063b9f3623f6606
Matthew <smoothdiamond45@gmail.com>
2026-02-04 21:09:32 +0000

    Update Store.java

--- Semester2/ShopV2.2/ShopV2.2/src/Store.java
+++ Semester2/ShopV2.2/ShopV2.2/src/Store.java
@@ -1,17 +1,11 @@
-/**
- * The store class saves all the entered products into a primitive array.
- *
- * @author Siobhan Drohan, Mairead Meagher
- * @version 2.2
- */
+import java.util.ArrayList;
 
 public class Store {
 
-    private Product[] products;
-    private int total = 0;  //dual purpose. 1) number of items stored in array, 2) next available index location
+    private ArrayList<Product> products;
 
-    public Store(int numberItems) {
-        products = new Product[numberItems];
+    public Store() {
+        products = new ArrayList<Products>();
     }
 
     private boolean isFull() {
@@ -22,48 +16,23 @@ public class Store {
         return total == 0;
     }
 
-    /**
-     * If there is space available, add the product object, passed as a parameter, to the array.
-     *
-     * @param product Product object to be added to the array.
-     * @return Status of the add; true for success, false for fail.
-     */
-    public boolean add(Product product) {
-        if (isFull()) {
-            return false;
-        } else {
-            products[total] = product;
-            total++;
-            return true;
-        }
+
+    public boolean add (Product product){
+        return products.add (product);
     }
 
-    /**
-     * This method builds and returns a String containing all the products in the array.
-     * For each product in the array, the associated index number is included.
-     * If no products are stored in the array, the String "No products in the store" is returned.
-     *
-     * @return A String containing all the products in the array, or "No products in the store",
-     * if empty.
-     */
-    public String listProducts() {
-        if (isEmpty()) {
-            return "No products in the store";
+    public String listProducts(){
+        if (products.isEmpty()) {
+            return "No products in store";
         } else {
             String listOfProducts = "";
-            for (int i = 0; i < total; i++) {
-                listOfProducts += i + ": " + products[i] + "\n";
+            for (int i = 0; i < products.size(); i++) {
+                listOfProducts += i + ": " + products.get(i) + "\n";
             }
             return listOfProducts;
         }
     }
 
-    /**
-     * This method returns the cheapest product in the array.
-     * If no products are stored in the array, null is returned.
-     *
-     * @return The cheapest Product in the array or null, if no products are added yet.
-     */
     public Product cheapestProduct() {
         if (!isEmpty()) {
             Product cheapestProduct = products[0];
@@ -77,24 +46,14 @@ public class Store {
         }
     }
 
-    /**
-     * This method builds and returns a String containing all the products in the array
-     * that are in the current product line.
-     * <p>
-     * For each product added to the String, the associated index number is included.
-     *
-     * @return A String containing all the products in the array, or "No products are in our current
-     * product line", if none in the current line.  If no products are stored in the array, the
-     * returned String indicates this.
-     */
     public String listCurrentProducts() {
         if (isEmpty()) {
             return "No Products in the store";
         } else {
             String listOfProducts = "";
             for (int i = 0; i < total; i++) {
-                if (products[i].isInCurrentProductLine())
-                    listOfProducts += i + ": " + products[i] + "\n";
+                if (products.get(i).isInCurrentProductLine())
+                    listOfProducts += i + ": " + products(i) + "\n";
             }
             if (listOfProducts.equals("")){
                 return "No Products are in our current product line";
@@ -105,37 +64,15 @@ public class Store {
         }
     }
 
-    /**
-     * This method returns the average product price of all the products in the array.
-     * If no products are stored in the array, the value returned is -1.
-     *
-     * @return The average product price, or -1 if no products exist.
-     */
     public double averageProductPrice() {
         if (!isEmpty()) {
             double totalPrice = 0;
-            for (int i = 0; i < total; i++) {
+            for (int i =0; i < total; i++){
                 totalPrice += products[i].getUnitCost();
             }
-            return totalPrice / total;
-        } else {
-            return -1;
         }
-
     }
 
-    /**
-     * This method builds and returns a String containing all the products in the array
-     * that are more expensive that a specific amount (passed as a parameter).
-     * <p>
-     * For each product added to the String, the associated index number is included.
-     * If no products are stored in the array, the returned String indicates this.
-     *
-     * @param price The value used to filter for products costing more than it.
-     * @return A String containing all the products in the array more expensive than the parameter value
-     * or "No Products are more expensive than: ", if none are more expensive.  If no products are
-     * in the array, the returned String contains "No products in store".
-     */
     public String listProductsAboveAPrice(double price) {
         if (isEmpty()) {
             return "No Products in the store";
